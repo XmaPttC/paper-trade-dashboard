21ba6c2e56934b00fc9202ce65a395be99b98cf2
XmaPttC <matt@palaya.co>
2025-06-13 16:54:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ html, body, .stApp, .block-container {
     color: #f1f5f9 !important;
 }
 section[data-testid="stSidebar"] {
-    background-color: #040608 !important;
+    background-color: #0b1015 !important;
     color: #f1f5f9 !important;
 }
 div[data-testid="stDataFrameContainer"] {
